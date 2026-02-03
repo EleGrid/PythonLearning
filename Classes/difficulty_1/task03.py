7c05c5a65c0f15b10108beb8e8023e9c789eb781
Elena Gridina <gridinaeqa@gmail.com>
2026-02-03 15:42:24 +0200

    Classes_task_3

--- Classes/difficulty_1/task03.py
+++ Classes/difficulty_1/task03.py
@@ -16,9 +16,8 @@ class Child:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
-    # Your code here
-    pass
+    def say_hello(self):
+        return f"Hello, my name is {self.name}!"
 
 
 class TestChildHello(unittest.TestCase):
